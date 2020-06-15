002ca3c64b16771a26ccb4e65eb8a3f061e1820f
Andrew Hansbrough <hansbroughku@gmail.com>
2020-06-15 15:42:38 -0500

    Added schema and seed files

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### Schema
+
+CREATE DATABASE burger_test_db;
+USE burger_test_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Winstead', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rode Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Peanut Butter Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Quarter Pounder with Cheese', true);
