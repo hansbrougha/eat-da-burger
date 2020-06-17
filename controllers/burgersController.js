5e7fbec4b2db5dcd85645e34e5ce5453c5065ade
Andrew Hansbrough <hansbroughku@gmail.com>
2020-06-16 21:44:43 -0500

    debugged controller

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function (req, res) {
+router.get("/", function (_, res) {
   burger.all(function (data) {
     var hbsObject = {
       burgers: data
