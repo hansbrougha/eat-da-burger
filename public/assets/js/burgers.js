907c58bd43c8886a7f5ce965c389be1b51c43570
Andrew Hansbrough <hansbroughku@gmail.com>
2020-06-16 21:45:22 -0500

    Debugged front end javascript

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,5 +1,5 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function () {
+$(document).ready(function () {
   $(".change-devoured").on("click", function (event) {
     var id = $(this).data("id");
     var newDevoured = $(this).data("newdevoured");
